05e81f14a9fa27cfa74b58edfa8b25854b7802b5
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-14 14:45:24 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Bahadır Tarcan on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
