67732c144c2437f11b1189857939fa67a23709e8
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-14 19:23:42 +0300

    Registration Screen UI added.

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewViewModel()
     
     var body: some View {
         NavigationView {
@@ -23,22 +22,16 @@ struct LoginView: View {
                 
                 // Login Form
                 Form {
-                    TextField("Email Adress", text: $email)
+                    TextField("Email Adress", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
-                     SecureField("Password", text: $password)
+                        .textInputAutocapitalization(.none)
+                    
+                    SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     
-                    Button {
+                    TLButton(title: "Log in",
+                             background: .blue) {
                         // Attempt log in
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color.blue)
-                            
-                            Text("Log in")
-                                .bold()
-                                .foregroundColor(Color.white)
-                        }
                     }
                     .padding()
                 
--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
@@ -15,6 +20,28 @@ struct RegisterView: View {
                        subtitle: "Start organizing todos",
                        angle: -15,
                        background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                TextField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account",
+                         background: .green) {
+                    // Attempt registration
+                }
+                         .padding()
+                
+            }
+            .offset(y: -50)
             
             Spacer()
         }
