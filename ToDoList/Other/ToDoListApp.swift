ffc5068d6c47b3f9f9fecb841b792eb3ef099024
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-14 15:41:16 +0300

    Rename View Folder.

--- ToDoList/Other/ToDoListApp.swift
+++ ToDoList/Other/ToDoListApp.swift
@@ -5,13 +5,19 @@
 //  Created by Bahadır Tarcan on 14.06.2023.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
