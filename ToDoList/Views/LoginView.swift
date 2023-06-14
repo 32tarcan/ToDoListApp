69676200cc8a1987e2741fda9519bb51392e7fd8
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-14 18:44:33 +0300

    LoginView Created.

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Hearder
+                HeaderView()
+                
+                // Login Form
+                Form {
+                    TextField("Email Adress", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                     SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // Attempt log in
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log in")
+                                .bold()
+                                .foregroundColor(Color.white)
+                        }
+                    }
+                    .padding()
+                
+                }
+                
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                    
+                    NavigationLink("Create An Account",
+                                   destination: RegisterView())
+                        
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+        }
+       
+        }
     }
 }
 
