d288f0f19b462d236d4f40b1a71b1354969a3b26
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-15 21:24:50 +0300

    Class names and init add.

---
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItemViewViewModel.swift
+//  ToDoList
+//
+//  Created by Bahadır Tarcan on 14.06.2023.
+//
+
+import Foundation
+
+/// ViewModel for single to do list item view (eacch row in items built)
+/// Primary tab
+class ToDoListItemViewViewModel: ObservableObject {
+    
+    init() {}
+}
