60964963502a72e61e9872701896523046695296
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-16 16:30:45 +0300

    Done Checkmarks functionality added.

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Bahadır Tarcan on 14.06.2023.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
@@ -12,6 +14,20 @@ class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+        
         
     }
 }
