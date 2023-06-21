48aef741190ea507706eaed17b56fc599aae0334
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-21 17:09:49 +0300

    UserNotifications added.

--- ToDoList/Other/ToDoListApp.swift
+++ ToDoList/Other/ToDoListApp.swift
@@ -7,6 +7,7 @@
 
 import FirebaseCore
 import SwiftUI
+import UserNotifications
 
 @main
 struct ToDoListApp: App {
@@ -14,10 +15,63 @@ struct ToDoListApp: App {
         FirebaseApp.configure()
     }
     
-    
     var body: some Scene {
         WindowGroup {
             MainView()
+                .onAppear {
+                    askForNotificationPermission()
+                }
         }
     }
+    
+    func askForNotificationPermission() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                dispathNotification()
+            case .denied:
+                
+                return
+            case .notDetermined:
+                notificationCenter.requestAuthorization(options: [.alert, .sound]) { didAllow, error in
+                    if didAllow {
+                        dispathNotification()
+                    } else {
+                        
+                    }
+                }
+            default:
+                return
+            }
+        }
+    }
+    
+    func dispathNotification() {
+        let identifier = "my-morning-notification"
+        let title = "Bugün Yapılacakları Unutma!"
+        let body = "Kaçırmış olduğun şeyler olabilir bir göz at"
+        let hour = 16
+        let minute = 56
+        let isDaily = true
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isDaily)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+        notificationCenter.add(request)
+    }
 }
+
